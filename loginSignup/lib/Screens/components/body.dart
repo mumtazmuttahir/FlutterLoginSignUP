2074eb49dd51881d042626e8046b7d254ddbcb3b
Muttahir Mumtaz <muttahirmumtaz@muttahirs-mbp>
2021-07-29 23:37:16 +0200

    add 2 more scripts

--- loginSignup/lib/Screens/components/body.dart
+++ loginSignup/lib/Screens/components/body.dart
@@ -1,18 +1,40 @@
+import 'dart:html';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:loginSignup/Screens/components/background.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_svg/avd.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 
 class Body extends StatelessWidget {
   @override
   Widget build (BuildContext context) {
-    Size size = MediaQuery.of (context).size;
-    return Container(
-      //I want to be as big as my parent allows (double.infinity)
-      // I want to be as big as the screen (MediaQuery).
-      height: size.height,
-      width: double.infinity,
-      child: Stack (
 
-      ),
-    );
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      //Column has children
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "WELCOME TO EDU",
+              style: TextStyle(fontWeight: FontWeight.bold),
+
+            ),
+            SvgPicture.asset ("assets/icons/chat.svg",
+            height: size.height * 0.45,
+            ),
+
+            // ignore: deprecated_member_use
+            FlatButton(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+              onPressed: () {},
+              child: Text("LOGIN"),
+            )
+          ],
+        ));
   }
 }
+
